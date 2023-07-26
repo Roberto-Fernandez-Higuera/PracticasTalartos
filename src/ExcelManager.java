6c83d6af250769b2b81038480b8e0191b54c8f98
Roberto <rfernh03@estudiantes.unileon.es>
2023-07-26 13:54:59 +0200

    Mucho avance en excel apoyo, falta hacer preguntas Comienzo excel capataces

--- src/ExcelManager.java
+++ src/ExcelManager.java
@@ -24,6 +24,9 @@ public class ExcelManager(){
     private static XSSFWorkbook wb;
     private XSSFSheet hojaIberdrola;
 
+    //Arraylist con los valores de todos los campos
+    private ArrayList<Apoyo> listaApoyos = new ArrayList<>();
+
     //MAPAS A UTILIZAR
     private HashMap<Integer, Mediciones> mapaMediciones = new HashMap<>();
 
@@ -55,11 +58,15 @@ public class ExcelManager(){
                Apoyo apoyoAnyadir = new Apoyo();
 
                /**
-                * APOYO
+                * ID FILA APOYO
                 */
                Integer id = fila.getRowNum() + 1;
                apoyoAnyadir.setIdApoyo(id);
-               apoyoAnyadir.setNumApoyo(fila.getCell(1).getNumericCellValue());
+
+               /**
+                * NUM APOYO
+                */
+                apoyoAnyadir.setNumApoyo(fila.getCell(1).getNumericCellValue());
 
                /**
                 * LONGITUD MANTENIMINETO
@@ -142,18 +149,200 @@ public class ExcelManager(){
                } else {
                    apoyoAnyadir.setObservaciones(fila.getCell(11).getStringCellValue());
                }
+               listaApoyos.add(apoyoAnyadir);
                mapaMediciones.put(id, apoyoAnyadir);
             }
         }
         return mapaMediciones;
     }
 
-    public void creacionExcelApoyosRealizados(){
-
+    public void ejecucionPrograma(){
+        creacionExcelApoyosRealizados();
+        //creacionExcelControlCapataces();
     }
 
-    public void creacionExcelControlCapataces(){
+    /**
+     * PARTE EXCEL APOYOS REALIZADOS
+     */
+    public void creacionExcelApoyosRealizados(){
+        FileOutputStream fileMod = null;
+        try{
+            fileMod = new FileOutputStream("EXCELS FINALES/APOYOS REALIZADOS NOMBRE.xlsx");
+        } FileNotFoundException e) {
+            System.out.println("Error al crear EXCEL DE APOYOS\n");
+            System.exit(-1);
+        }
 
+        //Método que va a crear y rellenar mi excel de apoyos
+        introducirValoresApoyos();
+
+        try {
+            wb.write(fileMod);
+        } catch (IOException e) {
+            System.out.println("Error al escribir EXCELL APOYOS\n");
+            System.exit(-1);
+        }
+
+        /**
+         * DUDA, PREGUNTAR SI SE UTILIZA EL SIGUIENTE TRY AQUÍ CUANDO CREEMOS LOS DOS EXCELS
+         */
+        try {
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error al cerrar fichero");
+            System.exit(-1);
+        }
+
+        try {
+            fileMod.close();
+        } catch (IOException e) {
+            System.out.println("Error al cerrar EXCEL");
+            System.exit(-1);
+        }
+    }
+
+    private void introducirValoresApoyos(){
+        int numApoyo = 0;
+        int longitudMantenimineto = 0;
+        int longitudLimpieza = 0;
+        int longitudApertura = 0;
+        int anomaliaVegetacion = 0;
+        int longitudCopa = 0;
+        int limpiezaBase = 0;
+        int podaCalle = 0;
+        int fijoSalida = 0;
+        Date dia = null;
+        String capataz = "";
+        int numDiasTrabajados = 0;
+        String pendienteTractor = "";
+        String trabajoRematado = "";
+        String observaciones = "";
+
+        for (int i = 0; i < listaApoyos.size(); i++){
+            Row fila = hoja.createRow(i);
+
+            numApoyo = listaApoyos.get(i).getNumApoyo();
+            Cell celdaNumApoyo = fila.createCell(0);
+            celdaNumApoyo.setCellValue(numApoyo);
+
+            longitudMantenimineto = listaApoyos.get(i).getLongitudMantenimineto();
+            Cell celdaLongitudMantenimiento = fila.createCell(1);
+            celdaLongitudMantenimiento.setCellValue(longitudMantenimineto);
+
+            longitudLimpieza = listaApoyos.get(i).getLongitudLimpieza();
+            Cell celdaLongitudLimpeza = fila.createCell(2);
+            celdaLongitudLimpeza.setCellValue(longitudLimpieza);
+
+            longitudApertura = listaApoyos.get(i).getLongitudApertura();
+            Cell celdaLongitudApertura = fila.createCell(3);
+            celdaLongitudApertura.setCellValue(longitudApertura);
+
+            anomaliaVegetacion = listaApoyos.get(i).getnumAnomalia();
+            Cell celdaAnomaliaVegetacion = fila.createCell(4);
+            celdaAnomaliaVegetacion.setCellValue(anomaliaVegetacion);
+
+            /**
+             * TODO Hablar con Inés sobre los campos que no están (CASOS ESPECIALES)
+             * Valores inicializados a 0 para ser cambiados posteriormente a mano
+             */
+            Cell celdaLongitudCopa = fila.createCell(5);
+            celdaLongitudCopa.setCellValue(longitudCopa);
+
+            limpiezaBase = listaApoyos.get(i).getLimpiezaBase();
+            Cell celdaLimpiezaBase = fila.createCell(6);
+            celdaLimpiezaBase.setCellValue(limpiezaBase);
+
+            /**
+             * CASO ESPECIAL
+             * Valores inicializados a 0 para ser cambiados posteriormente a mano
+             */
+            Cell celdaPodaCalle = fila.createCell(7);
+            celdaPodaCalle.setCellValue(podaCalle);
+
+            /**
+             * CASO ESPECIAL
+             * Valores inicializados a 0 para ser cambiados posteriormente a mano
+             */
+            Cell celdaFijoSalida = fila.createCell(8);
+            celdaFijoSalida.setCellValue(fijoSalida);
+
+            dia = listaApoyos.get(i).getDia();
+            Cell celdaDia = fila.createCell(9);
+            celdaDia.setCellValue(dia);
+
+            capataz = listaApoyos.get(i).getCapataz();
+            Cell celdaCapataz = fila.createCell(10);
+            celdaCapataz.setCellValue(capataz);
+
+            /**
+             * CASO ESPECIAL
+             * Valores inicializados a 0 para ser cambiados posteriormente a mano
+             */
+            Cell celdaNumDiasTrabajados = fila.createCell(11);
+            celdaNumDiasTrabajados.setCellValue(numDiasTrabajados);
+
+            /**
+             * CASO ESPECIAL
+             * Valores inicializados a 0 para ser cambiados posteriormente a mano
+             */
+            Cell celdaPendienteTractor = fila.createCell(12);
+            celdaPendienteTractor.setCellValue(pendienteTractor);
+
+            /**
+             * CASO ESPECIAL
+             * Valores inicializados a 0 para ser cambiados posteriormente a mano
+             */
+            Cell celdaTrabajoRematado = fila.createCell(13);
+            celdaTrabajoRematado.setCellValue(trabajoRematado);
+
+            observaciones = listaApoyos.get(i).getObservaciones();
+            Cell celdaObservaciones = fila.createCell(14);
+            celdaObservaciones.setCellValue(observaciones);
+        }
+    }
+
+    /**
+     * PARTE EXCEL CONTROL CAPATACES
+     */
+    public void creacionExcelControlCapataces(){
+        FileOutputStream fileMod2 = null;
+        try{
+            fileMod2 = new FileOutputStream("EXCELS FINALES/CONTROL CAPATACES NOMBRE.xlsx");
+        } FileNotFoundException e) {
+            System.out.println("Error al crear EXCEL DE CONTROL CAPATACES\n");
+            System.exit(-1);
+        }
+
+        //Método que va a crear y rellenar mi excel de capataces
+        introducirValoresCapataces();
+
+        try {
+            wb.write(fileMod2);
+        } catch (IOException e) {
+            System.out.println("Error al escribir EXCELL CAPATACES\n");
+            System.exit(-1);
+        }
+
+        try {
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error al cerrar fichero");
+            System.exit(-1);
+        }
+
+        try {
+            fileMod2.close();
+        } catch (IOException e) {
+            System.out.println("Error al cerrar EXCEL");
+            System.exit(-1);
+        }
+    }
+
+    public void introducirValoresCapataces(){
+        /**
+         * TODO Rellenado del excel capataces
+         * INTRODUCIR TODOS LOS VALORES
+         */
     }
 
 }
